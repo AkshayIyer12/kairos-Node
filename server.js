2e0a6eb8ad1ce9caf75e0f9907ba366eadc94a70
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-09 11:54:47 +0530

    Add func createParamsObject

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const routes = require('./routes')
 const multer = require('multer')()
-const fetch = require('node-fetch')
+const { enroll } = require('./helper.js')
+
 let app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -12,40 +13,25 @@ app.get('/', (req, res) => {
 })
 
 app.post(routes.upload, multer.any() ,(req, res) => {
+  const param = createParamsObject(req)
+  enroll(param)
+  .then(v => res.json({
+    status: 'success',
+    data: 'Image was successfully enrolled'
+  }))
+  .catch(err => res.json({
+    status: 'error',
+    message: err.message
+  }))
+})
+
+const createParamsObject = req => {
   const params = Object.create(null)
   let imageBase64 = Buffer.from(req.files[0].buffer).toString('base64')
   params.image = imageBase64
   params.subject_id = req.body.subjectId
   params.gallery_name = req.body.galleryName
-  let param = JSON.stringify(params)
-  enroll(param)
-    .then(v => res.json({
-      status: 'success',
-      data: 'Image was successfully enrolled'
-    }))
-    .catch(err => res.json({
-      status: 'error',
-      message: err.message
-    }))
-})
-
-const enroll = async (params) => {
-  let obj = {
-    method: 'POST',
-    body: params,
-    headers: {
-      'Content-Type': 'application/json',
-      'app_id': 'b9b14222',
-      'app_key': 'f86e1f973b0ea157ec2616b192693d15'
-    }
-  }
-  try {
-    let fetchedData = await fetch('http://api.kairos.com/enroll', obj)
-    let data = await fetchedData.json()
-    return data
-  } catch (err) {
-    throw new Error(err.status, err.message)
-  }
+  return JSON.stringify(params)
 }
 
 app.listen(3000, () => console.log('Server is running on 3000'))
