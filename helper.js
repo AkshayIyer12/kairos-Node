9d093b6c39b264393fe3b3ca7783775c1bf9526e
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-10 16:27:50 +0530

    Remove fetch & add rp

--- helper.js
+++ helper.js
@@ -1,14 +1,22 @@
-const fetch = require('node-fetch')
 const { createObject } = require('./postObject.js')
+const rp = require('request-promise')
 
-const uploadOrVerifyOrRecognize = async (params, method) => {
+const uploadOrVerifyOrRecognize = async (params, method, fileData) => {
+  let obj = createObject(params)
+  if (method === 'v2/media') {
+    obj = obj.setContentType('multipart/form-data')
+    obj = obj.setFormData(fileData)
+    delete obj.body
+  }
   try {
-    let data = await fetch(`http://api.kairos.com/${method}`, createObject(params))
-    let jsonData = await data.json()
-    if (method === 'enroll') return enrollImage(jsonData)
-    if (method === 'verify') return verifyImage(jsonData)
-    if (method === 'recognize') return recognizeImage(jsonData)
-    if (method === 'detect') return detectImage(jsonData)
+    let url = ''
+    obj.url = `http://api.kairos.com/${method}`
+    let data = await rp(obj)
+    if (method === 'enroll') return enrollImage(data)
+    if (method === 'verify') return verifyImage(data)
+    if (method === 'recognize') return recognizeImage(data)
+    if (method === 'detect') return detectImage(data)
+    console.log(data)
   } catch (err) {
     throw err
   }
@@ -41,17 +49,26 @@ const detectImage = data => {
     value: data
   }
 }
+
 const createParamsObject = req => {
   const params = Object.create(null)
-  let imageBase64 = Buffer.from(req.files[0].buffer).toString('base64')
-  params.image = imageBase64
+  if (req.body.method) {
+    params.image = Buffer.from(req.files[0].buffer).toString('base64')
+  }
+  if (req.body.vidMethod) {
+    if (req.body.vidMethod === 'v2/analytics') {
+      params.method = 'GET'
+    }
+    delete params.gallery_name
+  }
   if (req.body.method === 'enroll' || req.body.method === 'verify') {
     params.subject_id = req.body.subjectId
   }
-  if (req.body.method !== 'detect') {
+  if (req.body.method !== 'detect' && req.body.vidMethod !== 'v2/media') {
     params.gallery_name = req.body.galleryName
   }
-  return [JSON.stringify(params), req.body.method]
+  if (req.body.method) return [JSON.stringify(params), req.body.method, req.files[0]]
+  else return [JSON.stringify(params), req.body.vidMethod, req.files[0]]
 }
 
 module.exports = {
