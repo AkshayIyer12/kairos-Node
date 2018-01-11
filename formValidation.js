d091b9a29121a3375384d46b6d5a32eb6f6f85e3
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-11 16:07:19 +0530

    Add santizeRecognize function

--- formValidation.js
+++ formValidation.js
@@ -1,22 +1,42 @@
+const validateFile = file => {
+  if (file && (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png')) {
+    return file
+  } else {
+    throw Error('File not found or filetype not supported')
+  }
+}
 
 const sanitizeEnrollVerify = (body, file) => {
-  let err = ''
-  if (body && body.subjectId && body.galleryName && (body.method === 'enroll' || body.method === 'verify')) {
-    if (file && (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png')) {
-      return [body, file]
-    } else {
-      err = 'File not found or filetype not supported'
-      throw Error(err)
-    }
+  let newBody = {}
+  if (body && body.subjectId && body.galleryName) {
+    let newFile = validateFile(file)
+    newBody.subjectId = body.subjectId
+    newBody.galleryName = body.galleryName
+    newBody.method = body.method
+    return [newBody, newFile]
   } else {
-    err = 'Data not entered or Invalid Data entered'
-    throw Error(err)
+    throw Error('Data not entered or Invalid Data entered')
+  }
+}
+
+const sanitizeRecognize = (body, file) => {
+  let newBody = {}
+  if (body && body.galleryName) {
+    let newFile = validateFile(file)
+    newBody.galleryName = body.galleryName
+    newBody.method = body.method
+    return [newBody, newFile]
+  } else {
+    throw Error('Data not entered or Invalid Data entered')
   }
 }
 
 const sanitizeForm = req => {
   if (req.body.method === 'enroll' || req.body.method === 'verify') {
     return sanitizeEnrollVerify(req.body, req.files[0])
+  }
+  if (req.body.method === 'recognize') {
+    return sanitizeRecognize(req.body, req.files[0])
   } else {
     throw Error('Method not entered')
   }
