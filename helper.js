4e44720d1854f23b9b4dd02442702f02b3f7ed36
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-12 05:28:23 +0530

    Remove unnecessary functions and refactor sanitizeData function

--- helper.js
+++ helper.js
@@ -2,8 +2,8 @@ const { createObject } = require('./postObject.js')
 const fetch = require('node-fetch')
 const { sanitizeForm } = require('./formValidation.js')
 
-const uploadOrVerifyOrRecognize = async (req) => {
-  const [params, method, fileData] = createParamsObject(req)
+const showResults = async (req) => {
+  const [params, method, fileData] = sanitizeData(req)
   if (method === undefined) {
     throw Error('Method not selected')
   }
@@ -27,81 +27,31 @@ const uploadOrVerifyOrRecognize = async (req) => {
     }
     let data = await fetch(obj.url, obj)
     let jsonData = await data.json()
-    if (method === 'enroll') return enrollImage(jsonData)
-    if (method === 'verify') return verifyImage(jsonData)
-    if (method === 'recognize') return recognizeImage(jsonData)
-    if (method === 'detect') return detectImage(jsonData)
-    if (method === 'v2/media') return sendMedia(jsonData)
-    if (method === 'v2/analytics') return analyseMedia(jsonData)
+    if (Object.keys(jsonData)[0] === 'Errors') {
+      throw Error(jsonData['Errors'][0].Message)
+    }
+    return jsonData
   } catch (err) {
     throw err
   }
 }
 
-const analyseMedia = data => {
-  return {
-    message: 'Media Analysed',
-    value: data
+const sanitizeData = req => {
+  let [body, file, method] = sanitizeForm(req)
+  let obj = {
+    'enroll': 1,
+    'verify': 1,
+    'recognize': 1,
+    'detect': 1,
+    'v2/media': 0,
+    'v2/analytics': 0
   }
-}
-const sendMedia = data => {
-  return {
-    message: 'Media Posted',
-    value: data
+  if (obj[method]) {
+    body.image = Buffer.from(file.buffer).toString('base64')
   }
-}
-
-const enrollImage = data => {
-  return {
-    message: 'Face Enrolled',
-    value: data
-  }
-}
-
-const verifyImage = data => {
-  return {
-    message: 'Face Verified',
-    value: data
-  }
-}
-
-const recognizeImage = data => {
-  return {
-    message: 'Face Recognized',
-    value: data
-  }
-}
-
-const detectImage = data => {
-  return {
-    message: 'Face Detected',
-    value: data
-  }
-}
-
-const createParamsObject = req => {
-  const params = Object.create(null)
-  let [body, file] = sanitizeForm(req)
-  if (body.method) {
-    params.image = Buffer.from(file.buffer).toString('base64')
-  }
-  if (body.vidMethod) {
-    if (body.vidMethod === 'v2/analytics') {
-      params.method = 'GET'
-      params.id = body.videoID
-    }
-  }
-  if (body.method === 'enroll' || body.method === 'verify') {
-    params.subject_id = body.subjectId
-  }
-  if (body.method !== 'detect' && body.vidMethod !== 'v2/media' &&
-   body.vidMethod !== 'v2/analytics') {
-    params.gallery_name = body.galleryName
-  }
-  if (body.method) return [JSON.stringify(params), body.method, file]
-  else return [JSON.stringify(params), body.vidMethod, file]
+  return [JSON.stringify(body), method, file]
 }
 
 module.exports = {
-  uploadOrVerifyOrRecognize
+  showResults
 }
