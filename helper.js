58d8ace76c7d49a97f70859c9ed270642dc7aeb0
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-12 11:26:40 +0530

    Separate showResults into composable bundleUrl & createObject function

--- helper.js
+++ helper.js
@@ -1,30 +1,10 @@
-const { createObject } = require('./postObject.js')
+const { createPostObject } = require('./postObject.js')
 const fetch = require('node-fetch')
 const { sanitizeForm } = require('./formValidation.js')
 
 const showResults = async (req) => {
-  const [params, method, fileData] = sanitizeData(req)
-  if (method === undefined) {
-    throw Error('Method not selected')
-  }
-  let obj = createObject(params)
-  if (method === 'v2/media') {
-    obj = obj.setContentType('multipart/form-data')
-    obj = obj.setFormData(fileData)
-  }
   try {
-    let templateUrl = 'http://api.kairos.com'
-    let id
-    if (method === 'v2/analytics') {
-      id = JSON.parse(obj.body)
-    }
-    if (id && id.id) {
-      obj.url = `${templateUrl}/${method}/${id.id}`
-      delete obj.method
-      delete obj.headers['Content-Type']
-    } else {
-      obj.url = `${templateUrl}/${method}`
-    }
+    let obj = compose(bundleUrl, createObject)(req)
     let data = await fetch(obj.url, obj)
     let jsonData = await data.json()
     if (Object.keys(jsonData)[0] === 'Errors') {
@@ -34,6 +14,41 @@ const showResults = async (req) => {
   } catch (err) {
     throw err
   }
+}
+
+const compose = (f, g) => x => f(g(x))
+
+const bundleUrl = ([obj, method]) => {
+  let id = ''
+  if (method === 'v2/analytics') {
+    id = JSON.parse(obj.body)
+  }
+  return createUrl(obj)(method)(id)
+}
+
+const createUrl = obj => method => id => {
+  let templateUrl = 'http://api.kairos.com'
+  if (id && id.id) {
+    obj.url = `${templateUrl}/${method}/${id.id}`
+    delete obj.method
+    delete obj.headers['Content-Type']
+  } else {
+    obj.url = `${templateUrl}/${method}`
+  }
+  return obj
+}
+
+const createObject = req => {
+  const [params, method, fileData] = sanitizeData(req)
+  if (method === undefined) {
+    throw Error('Method not selected')
+  }
+  let obj = createPostObject(params)
+  if (method === 'v2/media') {
+    obj = obj.setContentType('multipart/form-data')
+    obj = obj.setFormData(fileData)
+  }
+  return [obj, method]
 }
 
 const sanitizeData = req => {
